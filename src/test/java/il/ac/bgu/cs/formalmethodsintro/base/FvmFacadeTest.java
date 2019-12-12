be551c7c262b5a6cb8db5ed4c80872d3c68b1a68
Rotem <rotba@post.bgu.ac.il>
2019-12-12 12:17:54 +0200

    nop

--- src/test/java/il/ac/bgu/cs/formalmethodsintro/base/FvmFacadeTest.java
+++ src/test/java/il/ac/bgu/cs/formalmethodsintro/base/FvmFacadeTest.java
@@ -3,6 +3,7 @@ package il.ac.bgu.cs.formalmethodsintro.base;
 import il.ac.bgu.cs.formalmethodsintro.base.goal.GoalStructure;
 import il.ac.bgu.cs.formalmethodsintro.base.transitionsystem.TSTransition;
 import il.ac.bgu.cs.formalmethodsintro.base.transitionsystem.TransitionSystem;
+import il.ac.bgu.cs.formalmethodsintro.base.util.GraphvizPainter;
 import il.ac.bgu.cs.formalmethodsintro.base.util.Pair;
 import org.junit.After;
 import org.junit.Before;
@@ -57,6 +58,80 @@ public class FvmFacadeTest {
         assertTrue(
                 fvm.isStateTerminal(p.getFirst(),p.getSecond())
         );
+    }
+
+    @Test
+    public void testInterleav() {
+        Pair<Pair<TransitionSystem, TransitionSystem>,TransitionSystem> p =T1T2T12();
+        System.out.println(GraphvizPainter.toStringPainter().makeDotCode(p.getFirst().getFirst()));
+        System.out.println(GraphvizPainter.toStringPainter().makeDotCode(p.getFirst().getSecond()));
+        System.out.println(GraphvizPainter.toStringPainter().makeDotCode(p.getSecond()));
+        assertEquals(
+                fvm.interleave(p.first.first, p.first.second), p.second
+        );
+    }
+
+    private Pair<Pair<TransitionSystem, TransitionSystem>, TransitionSystem> T1T2T12() {
+        TransitionSystem t1 = new TransitionSystem();
+        Object s10 = new Object();
+        Object s11 = new Object();
+        Object alpha1 = new Object();
+        Object a1 = new Object();
+        Object b1 = new Object();
+        t1.addInitialState(s10);
+        t1.addState(s11);
+        t1.addAction(alpha1);
+        t1.addAtomicProposition(a1);
+        t1.addAtomicProposition(b1);
+        t1.addToLabel(s10,a1);
+        t1.addToLabel(s11,b1);
+        t1.addTransition(new TSTransition(s10,alpha1,s11));
+
+        TransitionSystem t2 = new TransitionSystem();
+        Object s20 = new Object();
+        Object s21 = new Object();
+        Object alpha2 = new Object();
+        Object a2 = new Object();
+        Object b2 = new Object();
+        t2.addInitialState(s20);
+        t2.addState(s21);
+        t2.addAction(alpha2);
+        t2.addAtomicProposition(a2);
+        t2.addAtomicProposition(b2);
+        t2.addToLabel(s20,a2);
+        t2.addToLabel(s21,b2);
+        t2.addTransition(new TSTransition(s20,alpha2,s21));
+
+
+        TransitionSystem t12 = new TransitionSystem();
+        Pair<Object,Object>  p00  = new Pair<>(s10,s20);
+        Pair<Object,Object>  p01  = new Pair<>(s10,s21);
+        Pair<Object,Object>  p10  = new Pair<>(s11,s20);
+        Pair<Object,Object>  p11  = new Pair<>(s11,s21);
+        t12.addInitialState(p00);
+        t12.addState(p01);
+        t12.addState(p10);
+        t12.addState(p11);
+        t12.addAction(alpha2);
+        t12.addAtomicProposition(a1);
+        t12.addAtomicProposition(b2);
+        t12.addAtomicProposition(a2);
+        t12.addAtomicProposition(b2);
+        t12.addToLabel(p00,a1);
+        t12.addToLabel(p00,a2);
+        t12.addToLabel(p01,a1);
+        t12.addToLabel(p01,b2);
+        t12.addToLabel(p10,b1);
+        t12.addToLabel(p10,b1);
+        t12.addToLabel(p11,b1);
+        t12.addToLabel(p11,b2);
+
+        t12.addTransition(new TSTransition(p00,alpha1,p10));
+        t12.addTransition(new TSTransition(p00,alpha2,p01));
+        t12.addTransition(new TSTransition(p01,alpha1,p11));
+        t12.addTransition(new TSTransition(p10,alpha2,p11));
+
+        return new Pair<>(new Pair<>(t1,t2), t12);
     }
 
     private Pair<TransitionSystem, Object> getTSandNoTerminalState() {
